1e29759d0806482b028c722f266fbd3106968fe0
SunnyClouds333 <129523990+SunnyClouds333@users.noreply.github.com>
2023-09-28 21:28:08 +0300

    added football cards exercise

--- 11.Lists Basics/exercise/football_cards.py
+++ 11.Lists Basics/exercise/football_cards.py
@@ -3,16 +3,21 @@ cards_list = list(cards.split(" "))
 team_a = 11
 team_b = 11
 game_terminated = False
-set_of_list = set(cards_list)
+first_team_sent_off_players = []
+second_team_sent_off_players = []
 
-for card in set_of_list:
+for card in cards_list:
+    if card in first_team_sent_off_players or card in second_team_sent_off_players:
+        continue
     if "A" in card:
         team_a -= 1
+        first_team_sent_off_players.append(card)
     elif "B" in card:
         team_b -= 1
+        second_team_sent_off_players.append(card)
     if team_a < 7 or team_b < 7:
         game_terminated = True
-        continue
+        break
 
 print(f"Team A - {team_a}; Team B - {team_b}")
 if game_terminated:
